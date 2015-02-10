f93b0c5b0fa6a7d89deaf5bdfcdb3ce1bdbf4911
Jack Barker <jack.barker.au@gmail.com>
2015-02-10 01:50:16 +0000

    complete challenge 1

--- challenge1/start/app.js
+++ challenge1/start/app.js
@@ -4,8 +4,8 @@ var http = require('http'),
 
 http.createServer(function(request, response) {
 
-    // TODO: Find the ASYNCHRONOUS, NON-BLOCKING API for reading in a file.
-    fs.CHANGE_ME('./data.csv', 'utf-8', function(err, data) {
+    // The ASYNCHRONOUS, NON-BLOCKING API for reading in a file.
+    fs.readFile('./data.csv', 'utf-8', function(err, data) {
         var responseData = {};
 
         // Basic JS: Work with the data in the file, and create the response
@@ -17,13 +17,13 @@ http.createServer(function(request, response) {
             responseData[parts[0]] = parts[1];
         });
 
-        // TODO: How do we set the content type we're sending back?
+        // Set the content type we're sending back?
         response.writeHead(200, {
-            'CHANGEME':'CHANGEME'
+            'Content-Type': 'application/json'
         });
 
-        // TODO: How do we serialize responseData to a JSON string?
-        response.end(CHANGE_ME.CHANGE_ME(responseData));
+        // Serialize responseData to a JSON string?
+        response.end(JSON.stringify(responseData));
 
     });
 
